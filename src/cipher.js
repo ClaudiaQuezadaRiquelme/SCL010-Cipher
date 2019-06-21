0d53feac6af129e6e0126bd3d57cdb5a0b0a4183
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-06-21 12:52:05 -0400

    Mi primer add . y commit. Proyecto bastante avanzado

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,111 @@
+/* CIPHER CON MAYUSCULAS
+1. Recorrer string
+2. tomar el codigo ascii de la letra
+2.2 aplicarle la fórmula según el offset
+3. devolver letra con codigo
+4. transformar codigo ascii devuelto en letra
+5. mostrar letra
+*/
+
+/* CIPHER CON minusculas
+"a" comienza en 97
+"A-Z" => 65-90
+"a-z" => 97-122
+si x es >= 65 y <= 90, es mayúscula
+si x es >= 97 y <= 122 es minúscula
+
+  CIPHER CON NÚMEROS
+"0" comienza en 48
+"0-9" => 48-57
+si x es >= 48 y <= 57 es número
+
+  CARACTERES NO PERMITIDOS
+"" \ / : * ? " < > | ""
+EN ASCII => 92, 47, 58, 42, 63, 34, 60, 62, 124
+si x es igual a alguno de esos, no se puede codificar.
+indicar que son caracteres no permitidos y que ingrese caracteres válidos
+
+  CARACTERES ACEPTABLES
+Caracteres aceptables dejarlos igual
+*/
+
 window.cipher = {
-  encode: () => {
+  encode: (inputOffset, inputString) => {
     /* Acá va tu código */
-    
+    let nameCipher = "";// SI NO PONGO ESO, RETORNA Undefined+respuesta
+    //Recorrer string
+    for (let i = 0; i < inputString.length; i++) {
+      //tomar el codigo ascii de la letra
+      let codeChart = inputString.charCodeAt(i);
+
+      if (codeChart >= 65 && codeChart <= 90) {
+        //es mayúscula
+        nameCipher += String.fromCharCode((codeChart - 65 + inputOffset) % 26 + 65);
+      } else if (codeChart >= 97 && codeChart <= 122) {
+        //es minúscula
+        nameCipher += String.fromCharCode((codeChart - 97 + inputOffset) % 26 + 97);
+      } else if (codeChart >= 48 && codeChart <= 57) {
+        //es número
+        nameCipher += String.fromCharCode((codeChart - 48 + inputOffset) % 10 + 48);
+      } else if (codeChart === 92 || codeChart === 47 || codeChart === 58 || codeChart === 42 || codeChart === 63 || codeChart === 34 || codeChart === 60 || codeChart === 62 || codeChart === 124) {
+        //son caracteres no permitidos
+        alert('Hay caracteres no permitidos. No serán considerados.');
+      } else {
+        //Caracteres aceptables dejarlos igual
+        nameCipher += String.fromCharCode(codeChart);
+      }
+
+      //aplicarle la fórmula según el offset
+      //devolver letra con codigo
+      //transformar codigo ascii devuelto en letra
+
+      //nameCipher += String.fromCharCode((codeChart - 65 + inputOffset) % 26 + 65);
+    }
+    return nameCipher;
   },
-  decode: () => {
+  decode: (inputOffset, inputString) => {
     /* Acá va tu código */
+    let nameDecipher = "";// SI NO PONGO ESO, RETORNA Undefined+respuesta
+    //Recorrer string
+    for (let i = 0; i < inputString.length; i++) {
+      //tomar el codigo ascii de la letra
+      let codeChart = inputString.charCodeAt(i);
+      //console.log("codeChart " + codeChart);//BIEN
+      if (codeChart >= 65 && codeChart <= 90) {
+        //es mayúscula (FUNCIONA)
+        nameDecipher += String.fromCharCode((codeChart + 65 - inputOffset) % 26 + 65);
+      } else if (codeChart >= 97 && codeChart <= 122) {
+        //es minúscula (FUNCIONA)
+        //Lo utilizo como ejemplo para encontrar la fórmula para números.
+        //no sé por qué funciona, pero funciona.
+        //lo encontré en google.
+        nameDecipher += String.fromCharCode((codeChart + 85 - inputOffset) % 26 + 97);
+
+      } else if (codeChart >= 48 && codeChart <= 57) {
+        //es número (FUNCIONA)
+        //lo encontramos con pruebas con fuerza bruta hasta encontrar número correcto que se sume a codeChart desde 48 restando 1 en 1.
+        //ej:
+        //(codeChart + 47 - inputOffset) % 10 + 48 //NO FUNCIONA
+        //(codeChart + 46 - inputOffset) % 10 + 48 //NO FUNCIONA
+        //...
+        //(codeChart + 43 - inputOffset) % 10 + 48 //NO FUNCIONA
+        //(codeChart + 42 - inputOffset) % 10 + 48 //SÍ FUNCIONA
+        nameDecipher += String.fromCharCode((codeChart + 42 - inputOffset) % 10 + 48);
+
+      } else if (codeChart === 92 || codeChart === 47 || codeChart === 58 || codeChart === 42 || codeChart === 63 || codeChart === 34 || codeChart === 60 || codeChart === 62 || codeChart === 124) {
+        //son caracteres no permitidos (FUNCIONA)
+        alert("Hay caracteres no permitidos. No serán considerados.");
+      } else {
+        //Caracteres aceptables dejarlos igual (FUNCIONA)
+        nameDecipher += String.fromCharCode(codeChart);
+      }
+
+      //aplicarle la fórmula según el offset
+      //devolver letra con codigo
+      //transformar codigo ascii devuelto en letra
+
+      //nameDecipher += String.fromCharCode((codeChart + 65 - inputOffset) % 26 + 65);
+    }
+    return nameDecipher;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,221 @@
-/*Acá va tu código*/
+//DIV MOSTRAR - OCULTAR
+/*
+1. Guardar botón en una variable
+1.1 cerciórate que el botón tiene id
+2.AddEventListener para escuchar el click del botón
+3. ocultar div Welcome, mostrar div enter
+
+*/
+const btnYes = document.getElementById("yes");
+btnYes.addEventListener("click", ()=> {
+  //ocultar div Welcome, mostrar div enter
+  document.getElementById("welcome").style.display = "none";
+  document.getElementById("enter").style.display = "block";
+  document.getElementById("enterA").style.display = "block";
+  document.getElementById("enterB").style.display = "block";
+  //Mostrar botones ingresar.
+  //Agrego esto porque en una iteración que no reconocí, se borró uno de los botones ingresar y no sé por qué
+  document.getElementById("btnEnter1").style.display = "block";
+  document.getElementById("btnEnter2").style.display = "block";
+  //limpiar inputs
+  document.getElementById("nameA").value = "";
+  document.getElementById("offsetA").value = "";
+  document.getElementById("nameB").value = "";
+  document.getElementById("offsetB").value = "";
+
+});
+
+//DIV MOSTRAR - OCULTAR
+/*
+1. Guardar botón en una variable
+1.1 cerciórate que el botón tiene id
+3. ocultar div enterB [o enterA], mostrar div enterA [o enterB]
+2. AddEventListener para escuchar el click del botón
+4. guardar inputOffset
+5. guardar inputString
+6. llamar encode/decode y mostrar respuesta
+7. mostrar botones para volver
+*/
+
+const btnEnter1 = document.getElementById("btnEnter1");
+btnEnter1.addEventListener("click", ()=> {
+  //ocultar div enterB, mostrar div enterA
+  document.getElementById("enterB").style.display = "none";
+  //guardar inputOffset
+  let inputOffset = parseInt(document.getElementById("offsetA").value);
+  //guardar inputString
+  let inputString = document.getElementById("nameA").value;
+
+  //mostrar respuesta
+  document.getElementById("nameA").value = window.cipher.encode(inputOffset, inputString);
+  //mostrar botones para volver y ocultar boton ingresar
+  //CUIDADO PARA VOLVER A MOSTRAR btnEnter1
+  //tenemos un error de funcionamiento por esto
+  document.getElementById("btnEnter1").style.display = "none";
+  document.getElementById("copy1").style.display = "inline-block";
+  document.getElementById("comeBack1").style.display = "inline-block";
+  document.getElementById("restart1").style.display = "inline-block";
+  document.getElementById("cipher").style.display = "block";
+  //document.getElementByClassName("show").style.display = "block";//NO FUNCIONA => en google dicen que no existe, no está implementado.
+  /* //TAMPOCO FUNCIONA
+  let x = document.getElementByClassName("show");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "block";
+  };*/ //borré la class "show"
+
+
+});
+
+const btnEnter2 = document.getElementById("btnEnter2");
+btnEnter2.addEventListener("click", ()=> {
+  //ocultar div enterA, mostrar div enterB
+  document.getElementById("enterA").style.display = "none";
+  //guardar inputOffset
+  let inputOffset = parseInt(document.getElementById("offsetB").value);
+  //guardar inputString
+  let inputString = document.getElementById("nameB").value;
+  //mostrar respuesta
+  document.getElementById("nameB").value = window.cipher.decode(inputOffset, inputString);
+  //mostrar botones para volver y ocultar boton ingresar
+  //CUIDADO PARA VOLVER A MOSTRAR btnEnter1
+  //tenemos un error de funcionamiento por esto
+  document.getElementById("btnEnter2").style.display = "none";
+  document.getElementById("copy2").style.display = "inline-block";
+  document.getElementById("comeBack2").style.display = "inline-block";
+  document.getElementById("restart2").style.display = "inline-block";
+  document.getElementById("decipher").style.display = "block";
+  //document.getElementByClassName("show").style.display = "block";//NO FUNCIONA
+  /* //TAMPOCO FUNCIONA
+  let x = document.getElementByClassName("show");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "block";
+  };*/ //borré la class "show"
+
+});
+
+//BOTON COPIAR
+/*
+1. guardar botón en una variable
+2. eventListener cuando presiones el botón
+3. copiar con variable = getElementById(INPUT)
+4. usar   copyText.select(); y document.execCommand("copy");
+x. https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+*/
+//guardar botón en una variable
+const btnCopy1 = document.getElementById("copy1");
+//eventListener cuando presiones el botón
+btnCopy1.addEventListener("click", () => {
+  //copiar con variable = getElementById(INPUT)
+  let myCopy = document.getElementById("nameA");
+  //usar   copyText.select(); y document.execCommand("copy");
+  myCopy.select();/* Select the text field */
+  document.execCommand("copy");/* Copy the text inside the text field */
+});
+
+const btnCopy2 = document.getElementById("copy2");
+//eventListener cuando presiones el botón
+btnCopy2.addEventListener("click", () => {
+  //copiar con variable = getElementById(INPUT)
+  let myCopy = document.getElementById("nameB");
+  //usar   copyText.select(); y document.execCommand("copy");
+  myCopy.select();/* Select the text field */
+  document.execCommand("copy");/* Copy the text inside the text field */
+});
+
+//BOTÓN VOLVER
+/*
+A. guardar boton volver
+B. eventListener cuando presiones el botón
+1. Ocultar botones volver e id cipher/decipher.
+2. Mostrar boton ingresar.
+3. Mostrar enterX.
+4. limpiar inputs
+*/
+const btnComeBack1 = document.getElementById("comeBack1");
+btnComeBack1.addEventListener("click", () => {
+  //Ocultar botones volver e id cipher/decipher.
+  document.getElementById("copy1").style.display = "none";
+  document.getElementById("comeBack1").style.display = "none";
+  document.getElementById("restart1").style.display = "none";
+  document.getElementById("cipher").style.display = "none";
+  //Mostrar boton ingresar.
+  //duplico porque en una iteración que no reconocí, se borró uno de los botones ingresar y no sé por qué
+  document.getElementById("btnEnter1").style.display = "block";
+  document.getElementById("btnEnter2").style.display = "block";
+  //Mostrar enterX.
+  document.getElementById("enterB").style.display = "block";
+  //limpiar inputs
+  document.getElementById("nameA").value = "";
+  document.getElementById("offsetA").value = "";
+  document.getElementById("nameB").value = "";
+  document.getElementById("offsetB").value = "";
+});
+
+const btnComeBack2 = document.getElementById("comeBack2");
+btnComeBack2.addEventListener("click", () => {
+  //Ocultar botones volver e id cipher/decipher.
+  document.getElementById("copy2").style.display = "none";
+  document.getElementById("comeBack2").style.display = "none";
+  document.getElementById("restart2").style.display = "none";
+  document.getElementById("cipher").style.display = "none";
+  //Mostrar boton ingresar.
+  //duplico porque en una iteración que no reconocí, se borró uno de los botones ingresar y no sé por qué
+  document.getElementById("btnEnter2").style.display = "block";
+  document.getElementById("btnEnter1").style.display = "block";
+  //Mostrar enterX.
+  document.getElementById("enterA").style.display = "block";
+  //limpiar inputs
+  document.getElementById("nameA").value = "";
+  document.getElementById("offsetA").value = "";
+  document.getElementById("nameB").value = "";
+  document.getElementById("offsetB").value = "";
+});
+
+//BOTÓN REINICIAR
+/*
+A. guardar boton reiniciar
+B. eventListener cuando presiones el botón
+1. Ocultar botones volver e id cipher/decipher.
+2. limpiar inputs
+3. Ocultar div enter, enterX
+4. Mostrar welcome
+*/
+const btnRestart1 = document.getElementById("restart1");
+btnRestart1.addEventListener("click", () => {
+  //Ocultar botones volver e id cipher/decipher.
+  document.getElementById("copy1").style.display = "none";
+  document.getElementById("comeBack1").style.display = "none";
+  document.getElementById("restart1").style.display = "none";
+  document.getElementById("cipher").style.display = "none";
+  //limpiar inputs
+  document.getElementById("nameA").value = "";
+  document.getElementById("offsetA").value = "";
+  document.getElementById("nameB").value = "";
+  document.getElementById("offsetB").value = "";
+  //Ocultar div enter, enterX
+  document.getElementById("enterA").style.display = "none";
+  document.getElementById("enterB").style.display = "none";
+  document.getElementById("enter").style.display = "none";
+  //Mostrar welcome
+  document.getElementById("welcome").style.display = "block";
+});
+
+const btnRestart2 = document.getElementById("restart2");
+btnRestart2.addEventListener("click", () => {
+  //Ocultar botones volver e id cipher/decipher.
+  document.getElementById("copy2").style.display = "none";
+  document.getElementById("comeBack2").style.display = "none";
+  document.getElementById("restart2").style.display = "none";
+  document.getElementById("decipher").style.display = "none";
+  //limpiar inputs
+  document.getElementById("nameA").value = "";
+  document.getElementById("offsetA").value = "";
+  document.getElementById("nameB").value = "";
+  document.getElementById("offsetB").value = "";
+  //Ocultar div enter, enterX
+  document.getElementById("enterA").style.display = "none";
+  document.getElementById("enterB").style.display = "none";
+  document.getElementById("enter").style.display = "none";
+  //Mostrar welcome
+  document.getElementById("welcome").style.display = "block";
+});
