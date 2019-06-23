07c2703799fe44aa117992b7ccc12e2c9c3e4d27
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-06-22 23:06:25 -0400

    Test números y algunos caracteres válidos funcionan. Test caracteres inválidos no funciona, no sé arreglarlo.

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -17,6 +17,22 @@ describe('cipher', () => {
     it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
       assert.equal(cipher.encode(33,"abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg");
     });
+
+    it('debería retornar "3456789012" para "0123456789" con offset 33', () => {
+      assert.equal(cipher.encode(33,"0123456789"), "3456789012");
+    });
+
+    it('debería retornar " ¿¡-_#@%&=+" para " ¿¡-_#@%&=+" con offset 33 => algunos caracteres válidos', () => {
+      assert.equal(cipher.encode(33," ¿¡-_#@%&=+"), " ¿¡-_#@%&=+");
+    });
+
+    /*
+    it('debería retornar "" para "\/:*?"<>|" (Caracteres no válidos) con offset 33', () => {
+      assert.equal(cipher.encode(33,'//:*?"<>|'), "");
+    });*/ //NO FUNCIONA
+    //Intenté con caracteres hexadecimales, etc, pero no funciona tampoco.
+    // ¿no será por el alert que no funciona? puede ser.
+    // Tal vez si no estuviera el alert, funcionaría con caracteres especiales hexadecimales, &, etc.
   });
 
   describe('cipher.decode', () => {
@@ -31,6 +47,14 @@ describe('cipher', () => {
 
     it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
       assert.equal(cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"), "abcdefghijklmnopqrstuvwxyz");
+    });
+
+    it('debería retornar "0123456789" para "3456789012" con offset 33', () => {
+      assert.equal(cipher.decode(33,"3456789012"), "0123456789");
+    });
+
+    it('debería retornar " ¿¡-_#@%&=+" para " ¿¡-_#@%&=+" con offset 33 => algunos caracteres válidos', () => {
+      assert.equal(cipher.decode(33," ¿¡-_#@%&=+"), " ¿¡-_#@%&=+");
     });
   });
 
